7e394aee74af7a1225a2a9f499f05cef7c362de7
johnie261 <njorogejohn261@gmail.com>
2023-09-12 21:24:11 +0300

    Add product schema

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js"
 
 //data imports
 import User from "./models/User.js"
-import { dataUser } from "./data/index.js"
+import Product from './models/Product.js'
+import ProductStat from './models/ProductStat.js'
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js"
 
 //configuration
 
@@ -43,4 +45,6 @@ mongoose.connect(process.env.MONG0_URL, {
     app.listen(PORT, ()=> console.log(`Server Port: ${PORT}`))
 
     // User.insertMany(dataUser)
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
 }).catch((err) => console.log(`${err} did not connect`)) 
